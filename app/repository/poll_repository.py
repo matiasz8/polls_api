2e4a1b880435d2554d43232bcd3ac28ccf40a488
Matias Quiroga <nahuelq.78@gmail.com>
2021-12-23 11:59:18 -0300

    feat(mongo_service): load mongo connection on mongo service

--- app/repository/poll_repository.py
+++ app/repository/poll_repository.py
@@ -1,9 +1,8 @@
 import logging
 
-from app.core.config import MONGO_DB
+
 from app.services.mongo_service import MongoAPI
-from app.models.poll import PollListModel, PollModel, AnswerModel
-from app.exceptions.mongo_exception import MongoException
+from app.models.poll import PollListModel, PollModel
 
 
 logger = logging.getLogger(__name__)
@@ -12,19 +11,7 @@ logger = logging.getLogger(__name__)
 class PollRepository:
     polls_collection = "polls"
     answers_collection = "answers"
-
-    def __init__(self):
-        try:
-            logger.info("Connecting database...")
-            self.client = MongoAPI(mongo_db=MONGO_DB)
-            logger.info("Connection established with Mongo")
-        except KeyError as key_err:
-            logger.error(f"key error exception: {key_err}")
-            raise MongoException("key error exception.") from key_err
-        except ConnectionError as conn_err:
-            msg = f"Connection error on Mongo service. Details: {conn_err}"
-            logger.error(msg)
-            raise MongoException(message=msg) from conn_err
+    client = MongoAPI()
 
     def all_polls(self) -> PollListModel:
         polls = self.client.read(cursor=self.polls_collection)
@@ -40,6 +27,3 @@ class PollRepository:
 
     def create_poll(self, data: PollModel) -> str:
         return self.client.write(cursor=self.polls_collection, new_document=data.dict())
-
-    def create_answer(self, data: AnswerModel) -> str:
-        return self.client.write(cursor=self.answers_collection, new_document=data.dict())
--- app/services/mongo_service.py
+++ app/services/mongo_service.py
@@ -1,23 +1,36 @@
+import logging
 from datetime import datetime
 from bson import ObjectId
 from pymongo import MongoClient
 
-from app.core.config import MONGO_HOST
+from app.core.config import MONGO_HOST, MONGO_DB
 from app.exceptions.mongo_exception import MongoException
+
+logger = logging.getLogger(__name__)
 
 
 class MongoAPI():
-    def __init__(self, mongo_db):
-        self.client = MongoClient(host=MONGO_HOST)
-        self.cursor = self.client[mongo_db]
+
+    def __init__(self):
+        try:
+            logger.info("Connecting database...")
+            self.client = MongoClient(host=MONGO_HOST)
+            self.cursor = self.client[MONGO_DB]
+            logger.info("Connection established with Mongo")
+        except KeyError as key_err:
+            logger.error(f"key error exception: {key_err}")
+            raise MongoException("key error exception.") from key_err
+        except ConnectionError as conn_err:
+            msg = f"Connection error on Mongo service. Details: {conn_err}"
+            logger.error(msg)
+            raise MongoException(message=msg) from conn_err
 
     def read(self, cursor):
         self.collection = self.cursor[cursor]
         documents = self.collection.find()
-        output = [{item: str(data[item]) for item in data} for data in documents]
-        return output
+        return [{item: str(data[item]) for item in data} for data in documents]
 
-    def find_byid(self, cursor, id):
+    def find_by_id(self, cursor, id):
         self.collection = self.cursor[cursor]
         return self.collection.find_one({"_id" : ObjectId(id)})
 
